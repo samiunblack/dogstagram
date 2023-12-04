d43e224330db1588e59d77b213c562bbf07a5011
samiunblack <samiunblack@gmail.com>
2023-12-05 00:33:49 +0600

    add simple home page

--- dogstagram/settings.py
+++ dogstagram/settings.py
@@ -53,7 +53,7 @@ ROOT_URLCONF = 'dogstagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
