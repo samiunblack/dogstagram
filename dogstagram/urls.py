50a8042628e3baf775878d46b720eff7c19b1956
samiunblack <samiunblack@gmail.com>
2023-12-06 17:40:38 +0600

    implement user delete function

--- dogstagram/urls.py
+++ dogstagram/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
-from user.views import register_request, profile
+from user.views import register_request, profile, delete_request
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,5 +9,6 @@ urlpatterns = [
     path("accounts/", include("django.contrib.auth.urls")),
     path("accounts/register/", register_request, name="register"),
     path("accounts/<str:username>/", profile, name="profile"),
+    path("accounts/delete/<str:username>/", delete_request, name="delete_profile"),
     path("post/", include("post.urls"))
 ]
--- user/views.py
+++ user/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, login
 from .forms import NewUserForm
 from django.contrib.auth.models import User
@@ -18,6 +18,14 @@ def register_request(request):
             
             return redirect("home")
     return render (request=request, template_name="registration/register.html", context={"register_form":form})
+
+def delete_request(request, username):
+    user = get_object_or_404(User, username=username)
+    
+    if user == request.user:
+        user.delete()
+    
+    return redirect("home")
 
 
 def profile(request, username):
