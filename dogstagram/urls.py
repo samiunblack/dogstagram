d6bc44d1decc6e0489f964c7e99d107280e5ce17
samiunblack <samiunblack@gmail.com>
2023-12-06 11:08:58 +0600

    add user authorization protocol

--- dogstagram/urls.py
+++ dogstagram/urls.py
@@ -17,11 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
-from user.views import register_request
+from user.views import register_request, profile
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name="home"),
     path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/register/", register_request, name="register")
+    path("accounts/register/", register_request, name="register"),
+    path("accounts/<str:username>/", profile, name="profile"),
 ]
--- user/views.py
+++ user/views.py
@@ -1,12 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
 from .forms import NewUserForm
-
+from django.contrib.auth.models import User
 
 def register_request(request):
+    form = NewUserForm()
     if request.method == "POST":
         form = NewUserForm(request.POST)
         if form.is_valid():
             form.save()
-    else:  
-        form = NewUserForm()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            
+            if user != None:
+                login(request, user)
+            
+            return redirect("home")
     return render (request=request, template_name="registration/register.html", context={"register_form":form})
+
+
+def profile(request, username):
+    try:
+        user = User.objects.get(username=username)
+        return render(request, 'user/profile.html', {"user": user, "admin": request.user})
+    except User.DoesNotExist:
+        return render(request, 'user/profile.html', {"user": None, "admin": None})
